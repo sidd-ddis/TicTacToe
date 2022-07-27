0dd094cbcd915e94a8dc1bb72bce821d3103c102
Sid <Siddhart8525@gmail.com>
2022-07-27 09:08:52 +0530

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class TicTacToe
+{
+    char[] board = new char[10];
+    public void Board()
+    {
+
+        for(int i=1; i<board.length; i++)
+        {
+            board[i]= '';
+
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+        TicTacToe obj = new TicTacToe();
+        obj.Board();
+        System.out.println("*********adding the Tic Tac Toe Game on Board***********");
+    }
+
+}
